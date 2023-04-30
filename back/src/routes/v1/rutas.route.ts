9a69d2e101e19a982c79b9e44fb152e0431df6b6
Carloslecval <carloslecval03@gmail.com>
2023-04-30 15:44:32 -0600

    old backend removed

--- back/src/routes/v1/rutas.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "@awaitjs/express";
-import { getRutaParadas, getRutas } from "../../controller/v1/rutas.controller.js";
-const router = Router();
-
-router.getAsync('/', getRutas);
-router.getAsync('/paradas', getRutaParadas);
-
-export default router;
